a45ff30521b4ab26317b2f9041eb080466a10641
brigerodev <62488289+lolhol@users.noreply.github.com>
2023-11-25 22:26:52 -0800

    init

---
+++ src/main/java/com/finder/ForgeFinder.java
@@ -0,0 +1,13 @@
+package com.finder;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "ForgeFinder", useMetadata=true)
+public class ForgeFinder {
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        // Soon (TM)
+    }
+}
